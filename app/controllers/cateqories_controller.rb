fa7766ab5fd4b4b4153520c4e4d951dfc455dde7
Simon Wathigo <wathigosimon@gmail.com>
2020-04-17 09:58:02 +0300

    Adds categories controller methods and views

--- app/controllers/cateqories_controller.rb
+++
@@ -1,2 +0,0 @@
-class CateqoriesController < ApplicationController
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
+  resources :categories, only: %i[create]
+  root 'categories#new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
