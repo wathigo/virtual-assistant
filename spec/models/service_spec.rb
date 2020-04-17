b9896a8e267aaeedd71df07478d9ba9a1b91d6a0
Simon Wathigo <wathigosimon@gmail.com>
2020-04-17 11:42:38 +0300

    Add tests for services model

--- spec/models/service_spec.rb
+++ spec/models/service_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Service, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:service) { FactoryBot.build(:category) }
+  describe 'General category attributes validations' do
+    it 'should be valid' do
+      assert service.valid?
+    end
+
+    it 'should not be valid' do
+      service.name = ''
+      assert !service.valid?
+    end
+  end
 end
