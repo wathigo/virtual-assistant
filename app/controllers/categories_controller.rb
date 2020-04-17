9d3d7172e6f6bee8f5b396d59114fea0248ca339
Simon Wathigo <wathigosimon@gmail.com>
2020-04-17 10:01:14 +0300

    Fixes linter errors

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,18 +1,18 @@
 class CategoriesController < ApplicationController
-    def new
-    end 
-    
-    def create
-        @category = Category.new(category_params)
-        if @category.save
-            redirect_back(fallback_location: root_path)
-        else
-            flash[:alert] = "Somethig went wrong"
-        end
-    end
+  def new; end
 
-    private
-    def category_params
-        params.require(:category).permit(:name)
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      redirect_back(fallback_location: root_path)
+    else
+      flash[:alert] = 'Somethig went wrong'
     end
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name)
+  end
 end
--- spec/requests/cateqories_request_spec.rb
+++ spec/requests/cateqories_request_spec.rb
@@ -1,5 +1,4 @@
 require 'rails_helper'
 
-RSpec.describe "Cateqories", type: :request do
-
+RSpec.describe 'Cateqories', type: :request do
 end
